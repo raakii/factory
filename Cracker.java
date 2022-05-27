a7ac5255e060edc8f2a06b18a51e7109a35bdcb4
raakii <98623537+raakii@users.noreply.github.com>
2022-05-27 20:25:35 +0000

    Update Cracker.java

--- Cracker.java
+++ Cracker.java
@@ -7,32 +7,7 @@ interface Cracker  {
    
 }
 
- class DictionaryCracker implements Cracker  {
-    public void CrackPassword(User user)  {
-        int count = 0;
-        try {
-            File myFile = new File("dico2.txt");
-            Scanner myReader = new Scanner(myFile);
-            while (myReader.hasNextLine()) {
-                String password = myReader.nextLine();
-                System.out.println(password);
-                count++;
-                if(user.authentificate(password)) {
-                    System.out.println("Password hacked successfully in " + count + " attempts : " + password);
-                    myReader.close();
-                    return;
-                }
-            }
-            System.out.println("Password not foud");
-            myReader.close();
-        }
-        catch (FileNotFoundException e) {
-            System.out.println("An error occured");
-            e.printStackTrace();
-        }
-    }
-}
-
+ 
 
 class BruteForceCracker implements Cracker {
 
